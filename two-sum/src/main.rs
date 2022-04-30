6a868727709996713b7502d43394fe892a38308b
Kotaro Abe <abekoh.bcky@gmail.com>
2022-04-30 10:19:01 +0900

    update two-sum

--- two-sum/src/main.rs
+++ two-sum/src/main.rs
@@ -1,7 +1,13 @@
+use std::collections::HashMap;
+
+trait TwoSum {
+    fn two_sum(&self, nums: Vec<i32>, target: i32) -> Vec<i32>;
+}
+
 struct Solution01;
 
-impl Solution01 {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+impl TwoSum for Solution01 {
+    fn two_sum(&self, nums: Vec<i32>, target: i32) -> Vec<i32> {
         for (i, n) in nums.iter().enumerate() {
             for (j, m) in nums.iter().enumerate() {
                 if i == j {
@@ -16,8 +22,29 @@ impl Solution01 {
     }
 }
 
+struct Solution02;
+
+impl TwoSum for Solution02 {
+    fn two_sum(&self, nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut mp = HashMap::new();
+        for (i, n) in nums.iter().enumerate() {
+            if let Some(j) = mp.get(&(target - n)) {
+                return vec![i as i32, *j as i32];
+            }
+            mp.insert(n, i);
+        }
+        return vec![];
+    }
+}
+
 fn main() {
-    println!("{:?}", Solution01::two_sum(vec![2, 7, 11, 15], 9));
-    println!("{:?}", Solution01::two_sum(vec![3, 2, 4], 6));
-    println!("{:?}", Solution01::two_sum(vec![3, 3], 6));
+    let mut solutions = Vec::<Box<dyn TwoSum>>::new();
+    solutions.push(Box::new(Solution01 {}));
+    solutions.push(Box::new(Solution02 {}));
+
+    for sol in solutions.iter() {
+        println!("{:?}", sol.two_sum(vec![2, 7, 11, 15], 9));
+        println!("{:?}", sol.two_sum(vec![3, 2, 4], 6));
+        println!("{:?}", sol.two_sum(vec![3, 3], 6));
+    }
 }
