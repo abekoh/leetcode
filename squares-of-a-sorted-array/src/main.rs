07bc63f996217bdf602201d7aedd0992f43bdd43
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-04 16:15:46 +0900

    tmp

---
+++ squares-of-a-sorted-array/src/main.rs
@@ -0,0 +1,36 @@
+#![allow(dead_code)]
+
+struct Solution {}
+
+impl Solution {
+    pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
+        let mut nums = nums.iter().map(|x| {
+            x * x
+        })
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            Solution::sorted_squares(vec![-4, -1, 0, 3, 10]),
+            vec![0, 1, 9, 16, 100]
+        );
+    }
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            Solution::sorted_squares(vec![-7, -3, 2, 3, 11]),
+            vec![4, 9, 9, 49, 121]
+        );
+    }
+}
+
+fn main() {
+    println!("leetcode template");
+}
