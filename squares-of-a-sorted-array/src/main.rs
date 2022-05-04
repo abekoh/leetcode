866cbe94f46c7c73c62362974e1ba7f5665ba10f
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-04 16:53:22 +0900

    update

--- squares-of-a-sorted-array/src/main.rs
+++ squares-of-a-sorted-array/src/main.rs
@@ -5,9 +5,24 @@ struct Solution {}
 
 impl Solution {
     pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
-        let mut nums: Vec<i32> = nums.iter().map(|x| {
-            x * x
-        }).collect();
+        let mut result = Vec::with_capacity(nums.len() as usize);
+        let mut first = 0;
+        let mut last = nums.len() - 1;
+        (0..nums.len() as usize).for_each(|_| {
+            if nums[first].abs() > nums[last].abs() {
+                result.push(nums[first] * nums[first]);
+                first += 1;
+            } else {
+                result.push(nums[last] * nums[last]);
+                last -= 1;
+            }
+        });
+        result.reverse();
+        result
+    }
+
+    pub fn sorted_squares_old(nums: Vec<i32>) -> Vec<i32> {
+        let mut nums: Vec<i32> = nums.iter().map(|x| x * x).collect();
         nums.sort_unstable();
         nums
     }
