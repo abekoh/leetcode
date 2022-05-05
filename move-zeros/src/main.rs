274b5fb115b53717396e7e0df8f61a2f4eacfcdb
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-05 13:20:17 +0900

    update

--- move-zeros/src/main.rs
+++ move-zeros/src/main.rs
@@ -3,8 +3,20 @@
 struct Solution {}
 
 impl Solution {
-    pub fn sum_two_numbers(x: i32, y: i32) -> i32 {
-        x + y
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let n = nums.len();
+        nums.retain(|&x| x != 0);
+        nums.extend(std::iter::repeat(0).take(n - nums.len()));
+    }
+
+    pub fn move_zeroes_old(nums: &mut Vec<i32>) {
+        let l = nums.len();
+        *nums = nums.iter().copied().filter(|x| *x != 0).collect();
+        *nums = nums
+            .iter()
+            .copied()
+            .chain([0].repeat(l - nums.len()))
+            .collect();
     }
 }
 
@@ -14,11 +26,19 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(Solution::sum_two_numbers(1, 2), 3);
+        let mut nums = vec![0, 1, 0, 3, 12];
+        Solution::move_zeroes(&mut nums);
+        assert_eq!(nums, vec![1, 3, 12, 0, 0]);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut nums = vec![0];
+        Solution::move_zeroes(&mut nums);
+        assert_eq!(nums, vec![0]);
     }
 }
 
 fn main() {
     println!("leetcode template");
 }
-
