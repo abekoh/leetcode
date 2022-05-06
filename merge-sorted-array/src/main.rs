4ebc195d203495dddf928f56cc7b146ddc6cefbb
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-06 13:39:03 +0900

    tmp

---
+++ merge-sorted-array/src/main.rs
@@ -0,0 +1,59 @@
+#![allow(dead_code)]
+
+// let m = m as usize;
+// let n = n as usize;
+// nums1.splice(m..(m + n), nums2.clone());
+
+struct Solution {}
+
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let m = m as usize;
+        let n = n as usize;
+        let mut j = 0 as usize;
+        for i in 0..(m + n) {
+            if nums1[i] > nums2[j] {
+                nums1[i + 1] = nums1[i];
+                nums1[i] = nums2[j];
+                j += 1;
+            } else if i > m {
+                nums1[i] = nums2[j];
+                j += 1;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let mut num1 = vec![1, 2, 3, 0, 0, 0];
+        let mut num2 = vec![2, 5, 6];
+        Solution::merge(&mut num1, 3, &mut num2, 3);
+        assert_eq!(num1, vec![1, 2, 2, 3, 5, 6]);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut num1 = vec![1];
+        let mut num2 = vec![];
+        Solution::merge(&mut num1, 1, &mut num2, 0);
+        assert_eq!(num1, vec![1]);
+    }
+
+    #[test]
+    fn example_3() {
+        let mut num1 = vec![0];
+        let mut num2 = vec![1];
+        Solution::merge(&mut num1, 0, &mut num2, 1);
+        assert_eq!(num1, vec![1]);
+    }
+}
+
+fn main() {
+    println!("leetcode template");
+}
+
