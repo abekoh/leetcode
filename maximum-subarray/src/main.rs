fceebc62a3ad5fda73457f1c53436614e25ee788
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-06 10:21:58 +0900

    add

---
+++ maximum-subarray/src/main.rs
@@ -0,0 +1,42 @@
+#![allow(dead_code)]
+
+struct Solution {}
+
+impl Solution {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let mut sum = i32::MIN;
+        for n in 1..=nums.len() {
+            for i in 0..=(nums.len() - n) {
+                sum = i32::max(sum, nums[i..(i + n)].iter().sum());
+            }
+        }
+        sum
+    }
+}
+
+#[cfg(test)]
+mod maximum_subarray_tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+            6
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(Solution::max_sub_array(vec![1]), 1);
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(Solution::max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+    }
+}
+
+fn main() {
+    println!("leetcode template");
+}
