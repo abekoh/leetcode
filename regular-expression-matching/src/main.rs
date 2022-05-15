e261cb7ed077c4ef9ee07fb4e811a52bb66cd30f
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-15 22:14:21 +0900

    tmp

--- regular-expression-matching/src/main.rs
+++ regular-expression-matching/src/main.rs
@@ -4,7 +4,73 @@ struct Solution {}
 
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
-        false
+        let mut s_idx = 0;
+        let mut p_idx = 0;
+        while s_idx < s.len() && p_idx < p.len() {
+            let pattern = Pattern::new(&s[p_idx..std::cmp::min(p_idx + 2, p.len())]);
+            p_idx += pattern.forward();
+            while s_idx < s.len() {
+                let c = s.chars().nth(s_idx).unwrap();
+                if pattern.match_with(c) {
+                    s_idx += 1;
+                    if !pattern.is_any() {
+                        break;
+                    }
+                } else {
+                    if pattern.is_any() {
+                        break;
+                    }
+                    return false;
+                }
+            }
+        }
+        if s_idx < s.len() || p_idx < p.len() {
+            return false;
+        }
+        true
+    }
+}
+
+struct Pattern {
+    pub c: char,
+    is_any: bool,
+}
+
+impl Pattern {
+    pub fn new(s: &str) -> Self {
+        if s.len() == 1 {
+            let c = s.chars().nth(0).unwrap();
+            return Pattern { c, is_any: false };
+        }
+        let c = s.chars().nth(0).unwrap();
+        let option = s.chars().nth(1).unwrap();
+        return Pattern {
+            c,
+            is_any: option == '*',
+        };
+    }
+
+    fn is_wild(&self) -> bool {
+        self.c == '.'
+    }
+
+    pub fn is_any(&self) -> bool {
+        self.is_any
+    }
+
+    pub fn forward(&self) -> usize {
+        if self.is_any {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    pub fn match_with(&self, c: char) -> bool {
+        if self.is_wild() {
+            return true;
+        }
+        c == self.c
     }
 }
 
