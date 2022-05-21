b5b69265434c6a394d5d8c2c3390c5e2b291e6df
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-21 18:10:16 +0900

    prepare

--- valid-parentheses/src/main.rs
+++ valid-parentheses/src/main.rs
@@ -1,3 +1,31 @@
+#![allow(dead_code)]
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(Solution::is_valid(String::from("()")), true);
+    }
+    #[test]
+    fn example_2() {
+        assert_eq!(Solution::is_valid(String::from("()[]{}")), true);
+    }
+    #[test]
+    fn example_3() {
+        assert_eq!(Solution::is_valid(String::from("(]")), false);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("leetcode template");
 }
