913b839d4fff838f8daa2d9d751662980ab747c9
abekoh <abekoh.bcky@gmail.com>
2022-05-30 21:43:31 +0900

    set problem

--- minimum-path-sum/src/main.rs
+++ minimum-path-sum/src/main.rs
@@ -1,3 +1,26 @@
+#![allow(dead_code)]
+
+struct Solution {}
+
+impl Solution {
+    pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            Solution::min_path_sum(vec![vec![1, 3, 1], vec![1, 5, 1], vec![4, 2, 1]]),
+            7
+        );
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("leetcode template");
 }
