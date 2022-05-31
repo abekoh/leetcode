30d059af1b983db3bf847f24a35008757c912e49
abekoh <abekoh.bcky@gmail.com>
2022-05-31 22:15:30 +0900

    add solution

--- count-good-triplets/src/main.rs
+++ count-good-triplets/src/main.rs
@@ -4,7 +4,24 @@ struct Solution {}
 
 impl Solution {
     pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
-        0
+        let mut res = 0;
+        for i in 0..arr.len() - 2 {
+            for j in i + 1..arr.len() - 1 {
+                if (arr[i] - arr[j]).abs() > a {
+                    continue;
+                }
+                for k in j + 1..arr.len() {
+                    if (arr[j] - arr[k]).abs() > b {
+                        continue;
+                    }
+                    if (arr[k] - arr[i]).abs() > c {
+                        continue;
+                    }
+                    res += 1;
+                }
+            }
+        }
+        res
     }
 }
 
