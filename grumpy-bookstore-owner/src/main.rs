54d6a25ca16d58250e27cd887f4128f8c06a2134
abekoh <abekoh.bcky@gmail.com>
2022-06-01 22:56:58 +0900

    success

--- grumpy-bookstore-owner/src/main.rs
+++ grumpy-bookstore-owner/src/main.rs
@@ -4,7 +4,31 @@ struct Solution {}
 
 impl Solution {
     pub fn max_satisfied(customers: Vec<i32>, grumpy: Vec<i32>, minutes: i32) -> i32 {
-        0
+        let mut st = 0;
+        let mut mx = 0;
+        for i in 0..(customers.len() + 1 - minutes as usize) {
+            let mut tmp = 0;
+            for j in i..(i + minutes as usize) {
+                if grumpy[j] == 1 {
+                    tmp += customers[j];
+                }
+            }
+            if tmp > mx {
+                st = i;
+                mx = tmp;
+            }
+        }
+        let mut res = 0;
+        for (i, c) in customers.iter().enumerate() {
+            if i >= st && i < st + minutes as usize {
+                res += c;
+            } else {
+                if grumpy[i] == 0 {
+                    res += c;
+                }
+            }
+        }
+        res
     }
 }
 
