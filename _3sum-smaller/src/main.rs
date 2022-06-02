bcf17fdf792046f5c9b20007ed50eac4e031af9c
abekoh <abekoh.bcky@gmail.com>
2022-06-02 23:30:54 +0900

    update

--- _3sum-smaller/src/main.rs
+++ _3sum-smaller/src/main.rs
@@ -4,70 +4,26 @@ struct Solution {}
 
 impl Solution {
     pub fn three_sum_smaller(nums: Vec<i32>, target: i32) -> i32 {
-        let mut nums = nums.clone();
-        nums.sort();
         if nums.len() < 3 {
             return 0;
         }
-        let mut i = 0;
-        let mut j = 1;
-        let mut k = 2;
-        let mut phase = 0;
-        let mut revert = false;
-        let mut res = 0;
-        while phase <= 2 {
-            if nums[i] + nums[j] + nums[k] < target {
-                if !revert {
-                    res += 1;
+        let mut nums = nums.clone();
+        nums.sort();
+
+        let mut res: i32 = 0;
+        let i = 0;
+        for k in (2..nums.len()).rev() {
+            for j in (1..nums.len() - 1).rev() {
+                if k <= j {
+                    continue;
                 }
-                revert = false;
-                match phase {
-                    0 => {
-                        if k < nums.len() - 1 {
-                            k += 1;
-                        } else {
-                            revert = true;
-                            phase += 1;
-                        }
-                    }
-                    1 => {
-                        if k - j > 1 {
-                            j += 1;
-                        } else {
-                            revert = true;
-                            phase += 1
-                        }
-                    }
-                    2 => {
-                        if j - i > 1 {
-                            i += 1;
-                        } else {
-                            revert = true;
-                            phase += 1;
-                        }
-                    }
-                    _ => {
-                        unreachable!();
+                for i in 0..j {
+                    if nums[i] + nums[j] + nums[k] < target {
+                        res += 1;
+                    } else {
+                        break;
                     }
                 }
-                continue;
-            } else {
-                revert = true;
-                match phase {
-                    0 => {
-                        k -= 1;
-                    }
-                    1 => {
-                        j -= 1;
-                    }
-                    2 => {
-                        i -= 1;
-                    }
-                    _ => {
-                        unreachable!();
-                    }
-                }
-                phase += 1;
             }
         }
         res
@@ -96,6 +52,11 @@ mod tests {
     #[test]
     fn example_4() {
         assert_eq!(Solution::three_sum_smaller(vec![1, -2, 2, 1, 0], 1), 4);
+    }
+
+    #[test]
+    fn example_5() {
+        assert_eq!(Solution::three_sum_smaller(vec![1, -1, 2, 0, 3, -2], 2), 10);
     }
 }
 
