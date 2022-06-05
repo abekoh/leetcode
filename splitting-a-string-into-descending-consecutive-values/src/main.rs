9d85a7d5ef69ab699bffef28ce4f0fc7ab237dd5
Kotaro Abe <abekoh.bcky@gmail.com>
2022-06-05 13:50:21 +0900

    init

--- splitting-a-string-into-descending-consecutive-values/src/main.rs
+++ splitting-a-string-into-descending-consecutive-values/src/main.rs
@@ -1,3 +1,39 @@
+#![allow(dead_code)]
+
+struct Solution {}
+
+impl Solution {
+    pub fn split_string(s: String) -> bool {
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            Solution::split_string(String::from("1234")), false
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            Solution::split_string(String::from("050043")), true
+        );
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(
+            Solution::split_string(String::from("9080701")), false
+        );
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("leetcode template");
 }
