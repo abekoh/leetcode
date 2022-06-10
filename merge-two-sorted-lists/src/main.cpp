8dc1347d9a849e148f4fa09e3d66b6c6b656ae71
Kotaro Abe <abekoh.bcky@gmail.com>
2022-06-11 00:29:20 +0900

    add test

--- merge-two-sorted-lists/src/main.cpp
+++ merge-two-sorted-lists/src/main.cpp
@@ -1,15 +1,30 @@
 #include "gtest/gtest.h"
 
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    int sum(int a, int b) {
-        return a + b;
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        auto node = new ListNode();
+        return node;
     }
 };
 
 TEST(GameFrameworks, Placeholder) {
     auto sol = new Solution;
-    EXPECT_EQ(sol->sum(2, 3), 5);
+    EXPECT_EQ(sol->mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))),
+                                 new ListNode(1, new ListNode(3, new ListNode(4)))),
+              new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))))
+    );
 }
 
 int main(int argc, char **argv) {
