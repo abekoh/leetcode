2c75214cc46c37238f6626d399a860756fded373
Kotaro Abe <abekoh.bcky@gmail.com>
2022-06-11 01:10:26 +0900

    update

--- merge-two-sorted-lists/src/main.cpp
+++ merge-two-sorted-lists/src/main.cpp
@@ -27,28 +27,19 @@ bool operator==(const ListNode &lh, const ListNode &rh) {
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
-        auto res = new ListNode();
-        auto cur = res;
-        while (true) {
-            if (list1 == nullptr) {
-                cur->next = list2;
-                break;
-            }
-            if (list2 == nullptr) {
-                cur->next = list1;
-                break;
-            }
-            if (list1->val < list2->val) {
-                cur->next = new ListNode(list1->val);
-                cur = cur->next;
-                list1 = list1->next;
-            } else {
-                cur->next = new ListNode(list2->val);
-                cur = cur->next;
-                list2 = list2->next;
-            }
+        if (list1 == nullptr) {
+            return list2;
         }
-        return res->next;
+        if (list2 == nullptr) {
+            return list1;
+        }
+        if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
     }
 };
 
