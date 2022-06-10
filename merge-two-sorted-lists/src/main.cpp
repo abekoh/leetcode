fc546ac591f482f955ea7d2c92416fb77ce72624
Kotaro Abe <abekoh.bcky@gmail.com>
2022-06-11 01:02:53 +0900

    add operator

--- merge-two-sorted-lists/src/main.cpp
+++ merge-two-sorted-lists/src/main.cpp
@@ -11,6 +11,19 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+bool operator==(const ListNode &lh, const ListNode &rh) {
+    if (lh.val != rh.val) {
+        return false;
+    }
+    if (lh.next != nullptr && rh.next != nullptr) {
+        return operator==(*lh.next, *rh.next);
+    }
+    if (lh.next == nullptr && rh.next == nullptr) {
+        return true;
+    }
+    return false;
+}
+
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
@@ -41,10 +54,11 @@ public:
 
 TEST(GameFrameworks, Placeholder) {
     auto sol = new Solution;
-    EXPECT_EQ(sol->mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))),
-                                 new ListNode(1, new ListNode(3, new ListNode(4)))),
-              new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))))
-    );
+    auto actual = sol->mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))),
+                                     new ListNode(1, new ListNode(3, new ListNode(4))));
+    auto expected = new ListNode(1,
+                                 new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))));
+    EXPECT_EQ(*actual, *expected);
 }
 
 int main(int argc, char **argv) {
