97e82491aa5840341644c6ab316d800462ec8cce
Kotaro Abe <abekoh.bcky@gmail.com>
2022-06-11 16:13:12 +0900

    update init

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+if [ -z "$1"]; then
+  echo "please set problem name"
+  exit 1
+fi
+
+NAME = $1
+
+cp -R ./__TEMPLATES/cpp/REPLACE_ME ./
+mv REPLACE_ME $NAME
+find $NAME -type f | sed -e "s/REPLACE_ME/$NAME/g"
