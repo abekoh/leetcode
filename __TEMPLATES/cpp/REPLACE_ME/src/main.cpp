a3df5045bb660d1222574492ab33157417930c0d
Kotaro Abe <abekoh.bcky@gmail.com>
2022-06-11 16:05:15 +0900

    add template

---
+++ __TEMPLATES/cpp/REPLACE_ME/src/main.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+
+using namespace std;
+
+class Solution {
+public:
+    static int add(int a, int b) {
+        return a + b
+    }
+};
+
+TEST(Tests, Example01) {
+    EXPECT_EQ(Solution::add(1, 2), 3);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
